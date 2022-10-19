7d7d17b95bae6033847d8dd1a00351d21ca3013c
Silver Fan <SilverF@tl.com>
2022-10-19 21:31:27 +0800

    update flag auto execute

--- sendmail.py
+++ sendmail.py
@@ -7,7 +7,7 @@ def sendmail():
     content["subject"] = "[System generated] Black Panther is on sale now !!"  #郵件標題
     content["from"] = "silverfankw@gmail.com" #寄件者
     content["to"] = ", ".join(["silverfankw@gmail.com", "tonylo19961128@gmail.com"]) #收件者
-    content.attach(MIMEText("This is not SPAM!!! Link should be https://www.emperorcinemas.com/en/ticketing/movie_detail/showing/1853"))  
+    content.attach(MIMEText("Go go go... https://www.emperorcinemas.com/en/ticketing/movie_detail/showing/1853"))  
 
     with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
         try:
--- test-crawl.py
+++ test-crawl.py
@@ -1,25 +1,45 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 from sendmail import sendmail
 
 target_class = "movie-item-title"
 keyword = 'Black Panther: Wakanda Forever'
+track_interval = 300 # seconds as unit
+cont_loop = True
+
+def toMillisecond(epoch):
+    return int(round(epoch) * 1000)
 
 def handle_content(ctx):
+    global cont_loop 
+
     soup = BeautifulSoup(ctx, 'html.parser')
     results = soup.find_all("div", {"class": target_class})
-    print(results)
+    # print(results)
     if any(keyword in result for result in results):
         print(f"'{keyword}' found.")
         sendmail()
+        global cont_loop 
+        cont_loop = False
     else:
         print(f"'{keyword}' not found.")
 
+current_time = toMillisecond(time.time())
+time_end = toMillisecond(time.time() + 60 * 60 * 24)
+print(current_time, time_end)
 
-response = requests.get(f'https://www.emperorcinemas.com/en/ticketing/by_movie')
-# response = requests.get(f'https://www.emperorcinemas.com/en/ticketing/upcoming')
+while current_time < time_end and cont_loop:
+    response = requests.get(f'https://www.emperorcinemas.com/en/ticketing/by_movie')
+    # response = requests.get(f'https://www.emperorcinemas.com/en/ticketing/upcoming')
 
-if response.ok:
-    handle_content(response.content)
-else:
- print(response.status_code + " " + response.text)
+    if response.ok:
+        handle_content(response.content)
+    else:
+        print(response.status_code + " " + response.text)
+
+    time.sleep(track_interval)
+    current_time = toMillisecond(time.time())
+    print(current_time, time_end)
+
+print("End of tracking loop.")
